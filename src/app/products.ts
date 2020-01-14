50021bbd59db4dea4b26ea841d77823ea0211e68
chrmart5 <chrmart5@gmail.com>
2020-01-13 20:39:18 -0700

    Initial commit

---
+++ src/app/products.ts
@@ -0,0 +1,27 @@
+export const products = [
+  {
+    name: 'Phone XL',
+    price: 799,
+    description: 'A large phone with one of the best screens',
+    image: 'assets/images/test.png',
+  },
+  {
+    name: 'Phone Mini',
+    price: 699,
+    description: 'A great phone with one of the best cameras',
+    image: ''
+  },
+  {
+    name: 'Phone Standard',
+    price: 299,
+    description: '',
+    image: '',
+  }
+];
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
